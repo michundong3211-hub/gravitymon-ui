373df26eb44ff2f32592d8ec6a4c1bd8f601250b
michundong3211 <michundong3211@gmail.com>
2025-11-04 16:27:10 +0800

    15. 菜单删除

--- src/components/BsMenuBar.vue
+++ src/components/BsMenuBar.vue
@@ -1,107 +1,13 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
     <div class="container-fluid align-center">
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbar"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
 
       <div class="navbar-brand">
         {{ brand }}
       </div>
 
-      <div class="vr d-none d-lg-flex h-200 mx-lg-2 text-white"></div>
-
-      <div class="collapse navbar-collapse" id="navbar">
-        <ul class="navbar-nav">
-          <!-- 
-                        The drop-downs here are in conflict with the Vue so that dropdowns are not always closed. 
-                        This is a known issue with BS5 and Vue...
-                    -->
-          <template v-for="(item, index) in items" :key="index">
-            <li v-if="!item.subs.length" class="nav-item">
-              <router-link
-                :class="[
-                  'nav-link',
-                  $router.currentRoute.value.path.split('/')[1] === item.path.split('/')[1]
-                    ? ' active fw-bold'
-                    : ''
-                ]"
-                :to="item.path"
-                :disabled="disabled"
-              >
-                <component
-                  v-if="item.icon !== undefined"
-                  :is="item.icon"
-                  width="1rem"
-                  height="1rem"
-                  style="color: white"
-                ></component>
-
-                {{ item.label }}
-              </router-link>
-            </li>
-            <li v-else class="nav-item dropdown">
-              <a
-                @click="menuClicked"
-                :class="[
-                  'nav-link',
-                  'dropdown-toggle',
-                  $router.currentRoute.value.path.split('/')[1] === item.path.split('/')[1]
-                    ? ' active fw-bold'
-                    : ''
-                ]"
-                :id="'navbarDropdown' + item.label"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-                data-bs-auto-close="true"
-                :disabled="disabled"
-              >
-                <component
-                  v-if="item.icon !== undefined"
-                  :is="item.icon"
-                  width="1rem"
-                  height="1rem"
-                  style="color: white"
-                ></component>
-
-                {{ item.label }}
-                <span
-                  v-if="item.badge !== undefined && item.badge() > 0"
-                  class="badge text-bg-danger rounded-circle"
-                  >{{ item.badge() }}</span
-                >
-              </a>
-              <ul class="dropdown-menu" :aria-labelledby="'navbarDropdown' + item.label">
-                <li v-for="dn in item.subs" :key="dn.path">
-                  <router-link
-                    @click="subMenuClicked"
-                    class="dropdown-item"
-                    :to="dn.path"
-                    :disabled="disabled"
-                    >{{ dn.label }}
-                    <span
-                      v-if="dn.badge !== undefined && dn.badge() > 0"
-                      class="badge text-bg-danger rounded-circle"
-                      >{{ dn.badge() }}</span
-                    ></router-link
-                  >
-                </li>
-              </ul>
-            </li>
-          </template>
-        </ul>
-      </div>
-
-      <div class="vr d-none d-lg-flex h-200 mx-lg-2 text-white"></div>
+      <!-- 保留这个是为了样式留白 -->
+      <div class="collapse navbar-collapse" id="navbar"></div>
 
       <div class="text-white">
         {{ config.mdns }}
@@ -143,7 +49,6 @@
 import { config, global } from '@/modules/pinia'
 import { onMounted, watch } from 'vue'
 import { storeToRefs } from 'pinia'
-import { items } from '@/modules/router'
 /**
  * Purpose: Provide an meny with dropdown options, dark mode toggle and spinner.
  *
@@ -190,11 +95,4 @@ const setMode = () => {
   }
 }
 
-const subMenuClicked = () => {
-  // Added to fix the issue with dropdown-menu shown after select due to conflict with VueJS and BootstrapJS
-  const dnList = document.getElementsByClassName('dropdown-menu show')
-  for (var i = 0; i < dnList.length; i++) {
-    dnList[i].classList.remove('show')
-  }
-}
 </script>
